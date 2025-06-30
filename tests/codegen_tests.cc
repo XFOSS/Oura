98251455aa09243624fd01ad2dc6f5eaa50ad3d4
Donald Filimon <155747516+underswitchx@users.noreply.github.com>
2025-06-30 08:53:12 -0400

    Revert "Add real lexer and simple codegen tests"

--- tests/codegen_tests.cc
+++ tests/codegen_tests.cc
@@ -1,45 +1,5 @@
-#include "ourolang/lexer.h"
-#include "ourolang/parser.h"
-#include "ourolang/ast.h"
 #include <cassert>
-#include <sstream>
-
-// Simple code generator that emits literals and identifiers
-static void emit_expr(const ouro::Expr& e, std::ostringstream& out) {
-    using namespace ouro;
-    if (std::holds_alternative<NumberExpr>(e.value)) {
-        out << std::get<NumberExpr>(e.value).value;
-    } else if (std::holds_alternative<StringExpr>(e.value)) {
-        out << '"' << std::get<StringExpr>(e.value).value << '"';
-    } else if (std::holds_alternative<IdentExpr>(e.value)) {
-        out << std::get<IdentExpr>(e.value).name;
-    } else if (std::holds_alternative<BinaryExpr>(e.value)) {
-        const auto& b = std::get<BinaryExpr>(e.value);
-        out << '(';
-        emit_expr(*b.left, out);
-        switch (b.op) {
-            case TokenType::PLUS: out << " + "; break;
-            case TokenType::MINUS: out << " - "; break;
-            case TokenType::MUL: out << " * "; break;
-            case TokenType::DIV: out << " / "; break;
-            default: out << ' '; break;
-        }
-        emit_expr(*b.right, out);
-        out << ')';
-    }
-}
-
 int main() {
-    const std::string src = "let x = 2 + 3;";
-    ouro::Lexer lex(src);
-    auto tokens = lex.tokenize();
-    ouro::Parser parser(tokens);
-    auto ast = parser.parse();
-    assert(!ast.empty());
-    std::ostringstream out;
-    const auto& stmt = *ast.front();
-    const auto& var = std::get<ouro::VarDeclStmt>(stmt.value);
-    emit_expr(*var.value, out);
-    assert(out.str() == "(2 + 3)");
-    return 0;
+  assert(true);
+  return 0;
 }
